9dfa5c286998289efea3f24406ab80a7e2974b5a
jiangyang <1140938202@qq.com>
2023-06-30 15:18:11 +0800

    fix

--- store/index.js
+++ store/index.js
@@ -5,6 +5,17 @@ Vue.use(Vuex);//vue的插件机制
 
 //Vuex.Store 构造器选项
 const store = new Vuex.Store({
-	setting:{},
+	state: {
+		setting:[],
+	},
+	mutations: {
+		pushSetting(state,data){
+			console.log('satate',state,data)
+			state.setting.push(data)
+		},
+		setSetting (state,data) {
+			state.setting = data;
+		}
+	}
 })
 export default store
